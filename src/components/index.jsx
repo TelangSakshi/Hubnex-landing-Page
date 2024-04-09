feef8da73a38d1a8edfeb4d5a50774adb86924e3
saksh <sakshimtelang.cse20f@kdkce.edu.in>
2024-04-09 09:41:59 +0530

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import { Text } from "./Text";
+import { Heading } from "./Heading";
+import { Img } from "./Img";
+import { Button } from "./Button";
+import { CheckBox } from "./CheckBox";
+export { Text, Heading, Img, Button, CheckBox };
